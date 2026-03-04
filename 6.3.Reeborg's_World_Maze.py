606b7eefb6de93f1b0bd0bc4297a16849f8980d4
zulaldogu <zulalbeyzadogu@gmail.com>
2026-03-04 22:16:10 +0300

    maze completed

--- 6.3.Reeborg's_World_Maze.py
+++ 6.3.Reeborg's_World_Maze.py
@@ -4,7 +4,7 @@ def turn_right():
     turn_left()
 
 
-while front_is_clear()
+while front_is_clear():
     move()
 turn_left()
 
@@ -16,3 +16,4 @@ while not at_goal():
         move()
     else:
         turn_left()
+print("Hello World")
