10bee6b09e8a291973e281d60b5ad09e3e452821
zulaldogu <zulalbeyzadogu@gmail.com>
2026-04-25 22:53:02 +0300

    learning OOP

--- day-16-start/main.py
+++ day-16-start/main.py
@@ -8,3 +8,10 @@ timmy.forward(100)
 my_screen = Screen()
 print(my_screen.canvheight)
 my_screen.exitonclick()
+
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name",["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric","Water","Fire"])
+table.align = "l"
+print(table)
