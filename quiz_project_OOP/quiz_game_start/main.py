10cda6363388d285c935aa3548d38be0f4ff756a
zulaldogu <zulalbeyzadogu@gmail.com>
2026-04-26 18:51:00 +0300

    added while loop

--- quiz_project_OOP/quiz_game_start/main.py
+++ quiz_project_OOP/quiz_game_start/main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
@@ -9,5 +10,7 @@ for question in question_data:
     new_question = Question(question_text,question_answer)
     question_bank.append(new_question)
 
+quiz = QuizBrain(question_bank)
 
-print(question_bank)
+while quiz.still_has_questions():
+    quiz.next_question()
