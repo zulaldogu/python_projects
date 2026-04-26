5d98f408c3066b9c9d773aebc60eae9eea44c8aa
zulaldogu <zulalbeyzadogu@gmail.com>
2026-04-26 18:52:29 +0300

    showing new questions

--- quiz_project_OOP/quiz_game_start/quiz_brain.py
+++ quiz_project_OOP/quiz_game_start/quiz_brain.py
@@ -7,3 +7,6 @@ class QuizBrain:
         current_question = self.question_list[self.question_number]
         self.question_number += 1
         input(f"Q.{self.question_number} {current_question.text} (True/False): ")
+
+    def still_has_questions(self):
+         return self.question_number < len(self.question_list)
