1010f31e9e53fd0fa6b58c622bd6ab1125d3a75d
zulaldogu <zulalbeyzadogu@gmail.com>
2026-04-26 17:32:56 +0300

    using __init__ function

--- quiz_project_OOP/main.py
+++ quiz_project_OOP/main.py
@@ -1,12 +1,10 @@
 class User:
-    pass
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
 
-user_1 = User()
-user_1.id = "001"
-user_1.username = "zülal"
-
+user_1 = User("001", "zülal")
 print(user_1.username)
 
-user_2 = User()
-user_2.id = "002"
-user_2.name = "beyza"
+user_2 = User("002", "beyza")
+print(user_2.username)
