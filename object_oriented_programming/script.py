ab7623c099f1b417ea118deca81dba06144fd849
zulaldogu <zulalbeyzadogu@gmail.com>
2026-05-02 17:31:16 +0300

    creating person class

--- object_oriented_programming/script.py
+++ object_oriented_programming/script.py
@@ -21,3 +21,16 @@ print(dog1.owner.name)
 owner2 = Owner("Sally", "122 Springfield Drive", "888-999")
 dog2 = Dog("Freya", "Greyhound", owner2)
 print(dog2.owner.name)
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def greet(self):
+        print(f"Hello, my name is {self.name} and I am {self.age} years old")
+
+person1 = Person("Alice", 30)
+person1.greet()
+
+person2 = Person("Bob", 42)
+person2.greet()
