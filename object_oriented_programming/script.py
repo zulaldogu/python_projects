aa3e000d436f0f4b2206d3870b5633a2233cf470
zulaldogu <zulalbeyzadogu@gmail.com>
2026-05-02 15:25:13 +0300

    combining objects

--- object_oriented_programming/script.py
+++ object_oriented_programming/script.py
@@ -1,17 +1,23 @@
 class Dog:
-    def __init__(self, name, breed):
+    def __init__(self, name, breed, owner):
         self.name = name
         self.breed = breed
+        self.owner = owner
 
     def bark(self):
         print("Whoof whoof")
 
-dog1 = Dog("Bruce", "Scottish Terrier")
-dog1.bark()
-print(dog1.name)
-print(dog1.breed)
+class Owner:
+    def __init__(self, name, address, contact_number):
+        self.name = name
+        self.address = address
+        self.phone_number = contact_number
 
-dog2 = Dog("Freya", "Greyhound")
-dog2.bark()
-print(dog2.name)
-print(dog2.breed)
+
+owner1 = Owner("Danny", "122 Springfield Drive", "888-999")
+dog1 = Dog("Bruce", "Scottish Terrier", owner1)
+print(dog1.owner.name)
+
+owner2 = Owner("Sally", "122 Springfield Drive", "888-999")
+dog2 = Dog("Freya", "Greyhound", owner2)
+print(dog2.owner.name)
