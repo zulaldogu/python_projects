15848bc9bf3fe3c93766b3c685bb783f4ba2b117
zulaldogu <zulalbeyzadogu@gmail.com>
2026-05-04 14:02:57 +0300

    protected and private methods

--- object_oriented_programming/script2.py
+++ object_oriented_programming/script2.py
@@ -38,11 +38,15 @@ class BankAccount:
         self.owner = owner
         self._balance = balance
     def deposit(self, amount):
-        if amount > 0:
+        if self._is_valid_amount(amount):
             self._balance += amount
-            print(f"{self.owner}'s new balance: ${self._balance}")
+            self.__log_transaction("deposit", amount)
         else:
             print("Deposit amount must be positive.")
+    def _is_valid_amount(self, amount):
+        return amount > 0
+    def __log_transaction(self, transaction_type, amount):
+        print(f"Logging {transaction_type} of ${amount}. New balance: ${self._balance}")
 
     @staticmethod
     def is_valid_interest_rate(rate):
