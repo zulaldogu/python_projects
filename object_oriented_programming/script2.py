c710145bbcb64d96c4ca39e6111bd06f86b6b8a1
zulaldogu <zulalbeyzadogu@gmail.com>
2026-05-04 13:51:04 +0300

    learning static methods

--- object_oriented_programming/script2.py
+++ object_oriented_programming/script2.py
@@ -1,5 +1,5 @@
 # Static Attributes
-from object_oriented_programming.script import user1
+from cmath import acos
 
 
 # A static attribute (sometimes called a class attribute) is an attribute that belongs to the class itself, not to any
@@ -22,3 +22,34 @@ user2 = User("sally123", "sally@gmail.com")
 print(User.user_count)
 print(user1.user_count)
 print(user2.user_count)
+
+# Static Methods
+
+# A static method in Python is a method that belongs to the class itself rather than any instance od the class
+# itself rather than any instance of the class.
+
+# To define a static method, we use the '@staticmethod' decorator
+
+# Static vs. Instance Method Example
+
+class BankAccount:
+    MIN_BALANCE = 100
+    def __init__(self, owner, balance = 0):
+        self.owner = owner
+        self._balance = balance
+    def deposit(self, amount):
+        if amount > 0:
+            self._balance += amount
+            print(f"{self.owner}'s new balance: ${self._balance}")
+        else:
+            print("Deposit amount must be positive.")
+
+    @staticmethod
+    def is_valid_interest_rate(rate):
+        return 0<= rate <= 5
+
+account = BankAccount("Alice", 500)
+account.deposit(200)
+
+print(BankAccount.is_valid_interest_rate(3))
+print(BankAccount.is_valid_interest_rate(10))
