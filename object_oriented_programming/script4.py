4a4ae598f1efe184fd11c67319ea765002cbb0b0
zulaldogu <zulalbeyzadogu@gmail.com>
2026-05-08 22:56:39 +0300

    learning Polymorphism

--- object_oriented_programming/script4.py
+++ object_oriented_programming/script4.py
@@ -52,3 +52,49 @@ print(car.__dict__)
 print(bike.__dict__)
 car.start()
 bike.start()
+
+#Polymorphism
+#Having multiple forms
+
+class Car:
+    def __init__(self, brand, model, year, number_of_doors):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.number_of_doors = number_of_doors
+
+    def start(self):
+        print("Car is starting")
+
+    def stop(self):
+        print("Car is stopping")
+
+class Motorcycle:
+    def __init__(self, brand, model, year):
+        self.brand = brand
+        self.model = model
+        self.year = year
+
+    def start_bike(self):
+        print("Motorcycle is starting")
+
+    def stop_bike(self):
+        print("Motorcycle is stopping")
+
+# Create list of vehicles to inspect
+vehicles = [
+    Car("Ford", "Focus", 2008, 5),
+    Motorcycle("Honda", "Scoopy", 2018),
+]
+# Loop through list of vehicles and inspect them
+for vehicle in vehicles:
+    if isinstance(vehicle, Car):
+        print(f"Inspecting {vehicle.brand}, {vehicle.model}, ({type(vehicle).__name__})")
+        vehicle.start()
+        vehicle.stop()
+    elif isinstance(vehicle, Motorcycle):
+        print(f"Inspecting {vehicle.brand}, {vehicle.model}, ({type(vehicle).__name__})")
+        vehicle.start_bike()
+        vehicle.stop_bike()
+    else:
+        raise Exception("Object is not a valid vehicle")
