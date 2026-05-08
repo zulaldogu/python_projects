0a0c545acf21310779afeaa03c3ad359fc313a84
zulaldogu <zulalbeyzadogu@gmail.com>
2026-05-08 22:13:31 +0300

    createing Car and Bike classes

--- object_oriented_programming/script4.py
+++ object_oriented_programming/script4.py
@@ -34,3 +34,21 @@ class Vehicle:
 
     def stop(self):
         print("Vehicle is stopping")
+
+class Car(Vehicle):
+    def __init__(self, brand, model, year, number_of_doors, number_of_wheels):
+        super().__init__(brand,model,year)
+        self.number_of_doors = number_of_doors
+        self.number_of_wheels = number_of_wheels
+
+class Bike(Vehicle):
+    def __init__(self, brand, model, year, number_of_wheels):
+        super().__init__(brand,model,year)
+        self.number_of_wheels = number_of_wheels
+
+car = Car("Ford", "Focus", 2008, 5, 4)
+bike = Bike("Honda", "Scoopy", 2018, 2)
+print(car.__dict__)
+print(bike.__dict__)
+car.start()
+bike.start()
