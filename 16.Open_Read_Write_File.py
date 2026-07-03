f75dbad207c2ce877c192c10328af5b3047ef348
zulaldogu <zulalbeyzadogu@gmail.com>
2026-07-03 19:33:44 +0300

    added code and txt file

--- 16.Open_Read_Write_File.py
+++ 16.Open_Read_Write_File.py
@@ -2,3 +2,6 @@ with open("my_file.txt") as file: # dont need to close the file
     contents = file.read()
     print(contents)
     # file.close()
+
+with open("my_file.txt", mode = "w") as file:
+    file.write("New text.")
