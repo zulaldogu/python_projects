062e371fabd0b9e63bbdc4cb610f0c9508f8c155
zulaldogu <zulalbeyzadogu@gmail.com>
2026-07-04 20:21:22 +0300

    understanding relative and absolute paths

--- 16.Open_Read_Write_File.py
+++ 16.Open_Read_Write_File.py
@@ -1,7 +1,7 @@
-with open("my_file.txt") as file: # dont need to close the file
+with open("C:/Users/zulal/OneDrive/Masaüstü/my_file.txt") as file: # dont need to close the file
     contents = file.read()
     print(contents)
     # file.close()
 
-with open("my_file.txt", mode = "a") as file: # w is for write, a is for append
-    file.write("\nNew text.")
+# with open("my_file.txt", mode = "a") as file: # w is for write, a is for append
+#    file.write("\nNew text.")
